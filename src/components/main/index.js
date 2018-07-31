5a0e14fc5e90d6d9c7e4cad0fc24f63f62626a8a
okajopel <okajopel@cisco.com>
2018-07-30 22:52:24 -0400

    Register directive

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 import template from './main.html';
 import sideNav from '../side-nav';
 import registrationForm from '../registration-form';
+import textFileReader from '../text-file-reader';
 import './main.scss';
 
 function mainComponent() {
@@ -15,7 +16,8 @@ function mainComponent() {
 export default angular
   .module('main.component', [
     sideNav,
-    registrationForm
+    registrationForm,
+    textFileReader
   ])
   .directive('mainComponent', mainComponent)
   .name;
